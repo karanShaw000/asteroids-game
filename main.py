b48ce6b2c4dd68150ffcf7a6d5c6ea68d66e5de5
karanShaw000 <shawkaran2002@gmail.com>
2025-02-08 21:05:55 +0530

    boot.dev required game is done. Can add new thing by myself

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
+import sys
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
+from player import Player
+from shot import Shot
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (drawable, updatable)
+    Asteroid.containers = (asteroids, drawable, updatable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
 
+        updatable.update(dt)
+
+        for aster in asteroids:
+            if aster.collisionChecker(player):
+                print("Game Over!")
+                sys.exit()
+
+            for shot in shots:
+                if aster.collisionChecker(shot):
+                    aster.split()
+                    shot.kill()
+
+
+        screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
